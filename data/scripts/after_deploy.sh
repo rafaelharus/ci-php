a872254dcd39081403d8d870ceb2bf24e812fa38
Rafael Harus <rafael.cruz@g.globo>
2023-11-14 21:42:11 -0300

    pipeline 1

---
+++ data/scripts/after_deploy.sh
@@ -0,0 +1,6 @@
+echo "LOCALE=pt_BR
+LOCALE_DEFAULT=pt_BR
+CURRENCY_DEFAULT=BRL" >> /home/u508598207/app/test-app/checkdetour/.env &&
+echo "LOCALE=en_US
+LOCALE_DEFAULT=en_US
+CURRENCY_DEFAULT=USD" >> /home/u508598207/app/test-app/checkdetour_us/.env
